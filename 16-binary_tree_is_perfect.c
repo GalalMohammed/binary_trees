8cf3074f4b98e730419389db9d9a0abd330c5666
GalalMohammed <galalmoh2001@gmail.com>
2023-08-03 23:54:54 +0300

    add binary_tree_is_perfect function

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,61 @@
+#include "binary_trees.h"
+
+/**
+ * helper - measures the height of a binary tree corresponding to its parent
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: returns height if tree is not NULL, 0 otherwise
+ */
+size_t helper(const binary_tree_t *tree)
+{
+	size_t leftHeight = 1, rightHeight = 1;
+
+	if (tree)
+	{
+		if (tree->left)
+			leftHeight = helper(tree->left) + 1;
+		if (tree->right)
+			rightHeight = helper(tree->right) + 1;
+		if (leftHeight > rightHeight)
+			return (leftHeight);
+		else
+			return (rightHeight);
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: balance factor, 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		return (helper(tree->left) - helper(tree->right));
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if (binary_tree_balance(tree) == 0)
+		{
+			if (!tree->left && !tree->right)
+				return (1);
+			return (binary_tree_is_perfect(tree->left)
+					&& binary_tree_is_perfect(tree->right));
+		}
+	}
+	return (0);
+}
